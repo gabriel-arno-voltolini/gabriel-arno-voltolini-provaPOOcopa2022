c4fe33e0d36a9cac69b6d44bf7f1edf77743a494
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2021-12-07 20:21:32 -0300

    ComissaoTecnica implementada

---
+++ Prova_03/src/Domain/Entities/CommissaoTecnica.java
@@ -0,0 +1,20 @@
+package Domain.Entities;
+
+import java.time.LocalDate;
+
+public class CommissaoTecnica extends Membro{
+    
+    private String funcao;
+
+    public CommissaoTecnica(String nome, String apelido, LocalDate dataNascimento) {
+        super(nome, apelido, dataNascimento);
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+}
