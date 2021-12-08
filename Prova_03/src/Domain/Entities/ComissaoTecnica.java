ac84fd2338c2f0fabbe647830b6f3fddca7b3d4d
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2021-12-07 21:20:08 -0300

    getGrauHidratacao implementado

--- Prova_03/src/Domain/Entities/ComissaoTecnica.java
+++ Prova_03/src/Domain/Entities/ComissaoTecnica.java
@@ -2,30 +2,40 @@ package Domain.Entities;
 
 import java.time.LocalDate;
 import java.util.Random;
-import java.util.concurrent.locks.Condition;
 
 import Enumerators.Conditions;
 
-public class ComissaoTecnica extends Membro{
-    
-    private String funcao;
+public class ComissaoTecnica extends Membro {
 
-    public ComissaoTecnica(String nome, String apelido, LocalDate dataNascimento) {
-        super(nome, apelido, dataNascimento);
-    }
+	private String funcao;
 
-    public String getFuncao() {
-        return funcao;
-    }
+	public ComissaoTecnica(String nome, String apelido, LocalDate dataNascimento) {
+		super(nome, apelido, dataNascimento);
+	}
 
-    public void setFuncao(String funcao) {
-        this.funcao = funcao;
-    }
+	public String getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
 
 	@Override
 	public char getGrauHidratacao() {
-		// TODO Auto-generated method stub
-		return 0;
+		double temperatura = this.getTemperatura();
+		int hidratacao = this.getHidratacao();
+		char grauHidratacao = ' ';
+
+		if (temperatura <= 37 && hidratacao >= 90) {
+			grauHidratacao = 'H';
+		} else if (temperatura >= 37.3 | hidratacao <= 85) {
+			grauHidratacao = 'D';
+		} else {
+			grauHidratacao = 'E';
+		}
+
+		return grauHidratacao;
 	}
 
 	@Override
@@ -36,18 +46,17 @@ public class ComissaoTecnica extends Membro{
 		double temperaturaAtual = this.getTemperatura();
 		int hidratacaoAtual = this.getHidratacao();
 
-		if(randomValue == 7 | randomValue == 8){
+		if (randomValue == 7 | randomValue == 8) {
 			temperaturaAtual += 0.1;
-			if(c.equals(Conditions.MATCH)){
+			if (c.equals(Conditions.MATCH)) {
 				hidratacaoAtual -= (hidratacaoAtual * 0.03);
 			}
 			if (c.equals(Conditions.TRAINING)) {
 				hidratacaoAtual -= (hidratacaoAtual * 0.01);
 			}
-		}
-		else if(randomValue == 9 | randomValue == 10){
+		} else if (randomValue == 9 | randomValue == 10) {
 			temperaturaAtual -= 0.1;
-			if(c.equals(Conditions.MATCH)){
+			if (c.equals(Conditions.MATCH)) {
 				hidratacaoAtual += (hidratacaoAtual * 0.02);
 			}
 			if (c.equals(Conditions.TRAINING)) {
@@ -57,5 +66,4 @@ public class ComissaoTecnica extends Membro{
 		this.setTemperatura(temperaturaAtual);
 		this.setHidratacao(hidratacaoAtual);
 	}
-
 }
