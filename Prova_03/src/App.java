cf9e90651f5a2a768e216e9b4f505c5fda8ceddc
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2021-12-07 21:42:50 -0300

    Logger implementado

--- Prova_03/src/App.java
+++ Prova_03/src/App.java
@@ -1,5 +1,11 @@
+import java.nio.file.Paths;
+
+import Logger.ApplicationLogger;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        var logger = new ApplicationLogger("teste.txt", Paths.get("").toString());
+        logger.writeToFile("Teste");
     }
 }
---
+++ Prova_03/src/Logger/ApplicationLogger.java
@@ -0,0 +1,67 @@
+package Logger;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ApplicationLogger {
+    private String fileName;
+    private String filePath;
+    private File file;
+
+    public ApplicationLogger(String fileName, String filePath) {
+        this.setFileName(fileName);
+        this.setFilePath(filePath);
+        this.createFile(file);
+    }
+
+    public void writeToFile(String content) {
+        try {
+            FileWriter writer = new FileWriter(this.getFilePath() + this.getFileName());
+            writer.write(content);
+            writer.close();
+            System.out.println("Arquivo alterado com sucesso.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    };
+
+    private void createFile(File file) {
+        try {
+            file = new File(this.getFilePath() + this.getFileName());
+            if (file.createNewFile()) {
+                System.out.println("Arquivo criado: " + file.getName());
+            } else {
+                System.out.println("Arquivo já existe");
+            }
+        } catch (IOException e) {
+            System.out.println("Algo de errado ocorreu na criação do aquivo");
+            e.printStackTrace();
+        }
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    private void setFile(File file) {
+        this.file = file;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    private void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    private void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
